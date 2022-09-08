fa0fc51602c9dd482b0dfbcbe3487fc4177aa76e
maisapacheco <maisasoarespacheco@gmail.com>
2022-09-07 22:41:20 -0300

    Resultado

---
+++ Exercicio 02.py
@@ -0,0 +1,24 @@
+#Digite cada linha abaixo noshelldo Python e informequais estão corretos e quais apresentam erro:
+
+
+#a=1
+#b=1a
+#c=a1
+#d=1.
+#e=.2
+#f=-.3
+#g='agua''limpa'
+#h="agua"" 
+#i="""teste"""
+
+#print(a) 1
+#print(b) SyntaxError
+#print(c) NameError
+#print(d) 1.0
+#print(e) 0.2
+#print(f) -0.3
+#print(g) agua"limpa SyntaxError
+#print(h) SyntaxError
+#print(i) teste
+
+#Corretos[a,d,e,f,i]
