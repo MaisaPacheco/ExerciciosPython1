0d0a812f91ac34d626aa1e1cdc306b06dca26472
maisapacheco <maisasoarespacheco@gmail.com>
2022-09-17 20:49:04 -0300

    resultados

---
+++ ex11.py
@@ -0,0 +1,8 @@
+#11) Programa Python no arquivo ex11.py: Este programa irá calcular a área de um quadrado. Peça para a pessoa informar a medida numérica de um lado do quadrado. E depois informe-lhe o valor da área do quadrado.
+
+lado_quadrado=input('O lado do quadrado é: ')
+Àrea=int(lado_quadrado)**2
+print(Àrea)
+
+
+
--- lista03_02.py
+++ lista03_02.py
@@ -1,8 +1,8 @@
 # 10) Colete o nome da pessoa, a cidade de nascimento dela, e o ano em que ela nasceu. Depois você irá mostrar os dados coletados em linhas diferentes. E também, deverá informar quantos anos a pessoa terá no ano 2030.
 
-nome = input("Nome completo")
-cidade = input("Naturalidade")
-ano = int(input("Ano de nascimento"))
+nome=input("Nome completo")
+cidade=input("Naturalidade")
+ano=int(input("Ano de nascimento"))
 
 print(f"{nome}, nasceu na cidade de {cidade}, em 2030 irá ter {2030-ano} anos")
 
