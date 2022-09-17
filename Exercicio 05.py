46f5bd42e574052e2a1198d68f0963fb86a55bd3
maisapacheco <maisasoarespacheco@gmail.com>
2022-09-17 18:19:59 -0300

    resultado

---
+++ Exercicio 05.py
@@ -0,0 +1,54 @@
+#Resolva estes problemas em Python, guardando os valores e seus resultados em variáveis diferentes.
+
+#a. Calcule a área de um quadrado cujo lado seja 2 cm.
+
+from threading import main_thread
+
+
+lado=2
+a=lado*lado
+print(a)
+
+#4
+
+#b. Uma mala custa R$120,00. Esta recebeu 5% de desconto. Quanto você irá pagar por ela.
+
+mala=120.00
+desconto=0.05
+b=(mala*(1-desconto))
+print(b)
+
+#114.0
+
+#c. Um carro está viajando a uma velocidade média de 100 Km/h, o trecho de viagem será 200 Km. Quantas horas irá demorar  a viagem.
+
+velocidade=100
+trecho=200
+c=(trecho/velocidade)
+print(c)
+
+#2.0
+
+#d. João tem 2 pirulitos, Maria 3 pirulitos e Sofia 1 pirulito. Calcule o total de pirulitos e sua média.
+
+joao=2
+maria=3
+sofia=1
+d=(joao+maria+sofia)
+dm=(d/3)
+
+print(d)
+print(dm)
+
+#6
+#2.0
+
+#e. Davi tem 13 anos e sua irmã tem 7 anos. Guarde na variável eh_mais_velho a verificação se a idade de Davi é maior que a idade de sua irmã.
+
+davi=13
+irma=7
+
+eh_mais_velho=(davi>irma)
+print(eh_mais_velho)
+
+#True
