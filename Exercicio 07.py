c428f3dbf0853acd0adae5597a7e2050d5229dae
maisapacheco <maisasoarespacheco@gmail.com>
2022-09-17 18:48:10 -0300

    resposta

---
+++ Exercicio 07.py
@@ -0,0 +1,16 @@
+#7)  Considere as seguintes variáveis:
+
+ovo = 3.4 
+caju = 12.4
+
+resposta  = ovo if 1 > 2 else caju 
+resposta = ovo if ovo > caju else caju 
+resposta = ovo if ovo < caju else caju 
+resposta = 100 if ovo + caju > 15 else 200 
+resposta = 100 if ovo == 3 else 0
+
+#12.4
+#12.4
+#3.4
+#100
+#0
