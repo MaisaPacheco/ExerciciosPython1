eae1e56e829768e7a1e524263d6367adcbb4938e
maisapacheco <maisasoarespacheco@gmail.com>
2022-09-17 18:40:05 -0300

    resposta

--- Exercicio 06.py
+++ Exercicio 06.py
@@ -1,1 +1,20 @@
-#
+#6) Qual será o valor de z? Qual seria outra forma de escrever este trecho de código?
+
+z = 3 
+z += 2 
+z *= 6 
+z /= 5
+
+print(z)
+
+#6.0
+
+z=4
+z=z+1.0
+z=z**2
+z=z-19
+
+
+print(z)
+
+#6.0
